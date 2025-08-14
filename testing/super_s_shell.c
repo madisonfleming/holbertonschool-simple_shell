d9a55af7e73f51875bc2204ff5cf4bc213b86429
madisonfleming <maddyyfleming@gmail.com>
2025-08-14 13:47:57 +1000

    Update

--- testing/super_s_shell.c
+++ testing/super_s_shell.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+int main(void)
+{
+    char command[1024];
+    char *argv[2];
+    int stat;
 
+    while (1)
+    {
+        printf("$ "); /* Prompt */
+
+        if (!fgets(command, sizeof(command), stdin))
+            return 0; /* Ctrl+D */
+
+        command[strcspn(command, "\n")] = '\0'; /* Remove newline */
+
+        argv[0] = command;
+        argv[1] = NULL;
+
+        if (fork() == 0)
+            execve(command, argv, NULL), perror("execve"), _exit(1);
+
+        wait(&stat); /* Wait for child */
+    }
+}
